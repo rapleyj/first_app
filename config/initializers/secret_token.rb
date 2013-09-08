94f09d6e3378cfd2a17cd164afbe4350bfd2618e
James Rapley <jrapley@groupon.com>
2013-09-08 13:30:54 -0500

    initialise repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a40af601782e1d05ad7712e8d5ee6b15d6af84314da6e1ca83b417f972d0d2c0ddc8ea7c260ac8ce183a19004b6dec9bb037ddacf635f64afb3ae54851c63d38'
